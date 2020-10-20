b8f5276c6eefb67b04d30470b3e6cf10e71a0a15
Andre Oliveira <oliveiraa80@gmail.com>
2020-10-20 08:03:58 -0300

    projeto iniciado, mostrando ola mundo apenas

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GitHub_Teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá Mundo!");
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
